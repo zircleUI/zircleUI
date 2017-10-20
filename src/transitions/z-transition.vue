3f39fb8c5947ca5e644bfb6e938337d947cc2218
zircleUI <juan.martin.muda@gmail.com>
2017-10-20 16:22:18 -0400

    Delete 'preserve-3d'. Scale3d set to scale

--- src/transitions/z-transition.vue
+++ src/transitions/z-transition.vue
@@ -1,78 +1,53 @@
-<template>
- <transition
-  :name='name' 
-  :mode="mode"
-  :css="false"
-  @before-enter="beforeEnter"
-  @enter="enter"
-  >
-      <slot></slot>
- </transition>
-</template>
-
 <script>
-import store from '../store/store'
 export default {
-  props: ['name'],
-  data () {
-    return {
-      state: store.state,
-      store: store
-    }
-  },
-  computed: {
-    direction () {
-      return this.state.mode
-    },
-    mode () {
-      if (this.direction === 'forward') {
-        return 'out-in'
-      } else {
-        return 'in-out'
-      }
-    }
-  },
-  methods: {
-    beforeEnter (el) {
-      if (this.name === 'zuit') {
-        if (this.direction === 'forward') {
-          if (el.title === this.state.currentView) {
-            el.style.opacity = 0
+  functional: true,
+  render: function (createElement, context) {
+    var data = {
+      props: {
+        name: 'zuit',
+        css: false
+      },
+      on: {
+        beforeEnter (el) {
+          el.style.opacity = 0
+        },
+        enter (el, done) {
+          var point = document.querySelector('#z-point')
+          if (context.parent.$zircleStore.state.mode === 'forward') {
+            el.style.animation = 'appear .5s forwards'
+            // point.style.transformStyle = 'preserve-3d'
+            point.style.transform = 'scale(' + context.parent.$zircleStore.state.position.scale + ') translate3d(' + context.parent.$zircleStore.state.position.Xi + 'px, ' + context.parent.$zircleStore.state.position.Yi + 'px, 0px)'
+            point.style.transition = 'transform .5s cubic-bezier(1, .04, .94, .93)'
+            done()
+            //  PREVIOUS VIEW ==>
+          } else {
+            el.style.opacity = 1
+            done()
           }
-        } else {
-          if (el.title === this.state.currentView) {
-            el.style.opacity = 0
+        },
+        leave (el, done) {
+          var point = document.querySelector('#z-point')
+          if (context.parent.$zircleStore.state.mode === 'forward') {
+            done()
+          } else {
+            // point.style.transformStyle = 'preserve-3d'
+            point.style.transform = 'scale(' + context.parent.$zircleStore.state.position.scale + ') translate3d(' + context.parent.$zircleStore.state.position.Xi + 'px, ' + context.parent.$zircleStore.state.position.Yi + 'px, 0px)'
+            point.style.transition = 'transform .5s ease-in-out'
+            done()
           }
         }
       }
-    },
-    enter (el, done) {
-      var point = document.querySelector('#point')
-      if (this.name === 'zuit') {
-        if (this.direction === 'forward') {
-          if (el.title === this.state.currentView) {
-            el.style.opacity = 1
-          }
-          point.style.transformStyle = 'preserve-3d'
-          point.style.transitionDuration = '.5s'
-          point.style.transitionTimingFunction = 'cubic-bezier(1, .04, .94, .93)' // 'cubic-bezier(0,0,.39,0)'
-          point.style.transform = 'scale3d(' + this.state.position.scale + ', ' + this.state.position.scale + ', ' + this.state.position.scale + ') translate3d(' + this.state.position.Xi + 'px, ' + this.state.position.Yi + 'px, 0px)'
-          done()
-        } else {
-          if (el.title === this.state.currentView) {
-            el.style.opacity = 1
-          }
-          point.style.transformStyle = 'preserve-3d'
-          point.style.transitionDuration = '.6s'
-          point.style.transitionTimingFunction = 'ease-in-out'
-          point.style.transform = 'scale3d(' + this.state.position.scale + ', ' + this.state.position.scale + ', ' + this.state.position.scale + ') translate3d(' + this.state.position.Xi + 'px, ' + this.state.position.Yi + 'px, 0px)'
-          done()
-        }
-      }
     }
+    return createElement('transition', data, context.children)
   }
 }
-</script>
+</script> 
 <style>
+@keyframes appear {
+  0% {opacity: 0;}
+  80% {opacity: 0;}
+  100% {opacity: 1;}
+}
 
 </style>
+
