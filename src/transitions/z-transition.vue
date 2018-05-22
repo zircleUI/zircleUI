ac76de07bee3c5e325a615f33d4cbd8d4819bbbd
zircleUI <juan.martin.muda@gmail.com>
2018-05-22 13:13:31 -0400

    WIP transitions trying to improve go back transitions

--- src/transitions/z-transition.vue
+++ src/transitions/z-transition.vue
@@ -6,45 +6,63 @@ export default {
       props: {
         name: 'zuit',
         css: false,
-        tag: 'section'
+        tag: 'section',
+        type: 'animation'
       },
       on: {
+        beforeEnter (el) {
+          // console.log(document.styleSheets[0])
+          // document.styleSheets[0].insertRule('.have-border { border: 1px solid black;}', 0)
+          // document.styleSheets[0].deleteRule(0)
+          if (context.parent.$zircle.getNavigationMode() === 'forward') {
+          } else {
+          }
+        },
         enter (el, done) {
           var point = document.getElementById('z-point')
+          // var container = document.getElementById('z-p')
           if (context.parent.$zircle.getNavigationMode() === 'forward') {
-            point.style.willChange = 'transform'
+            // container.style.transformOrigin = context.parent.$zircle.getCurrentPosition().X + 'px ' + context.parent.$zircle.getCurrentPosition().Y + 'px'
+            // point.style.transformOrigin = context.parent.$zircle.getCurrentPosition().X + 'px ' + context.parent.$zircle.getCurrentPosition().Y + 'px'
+            // container.style.transform = ''
             point.style.transform = 'scale(' + context.parent.$zircle.getCurrentPosition().scale + ') translate3d(' + context.parent.$zircle.getCurrentPosition().Xi + 'px, ' + context.parent.$zircle.getCurrentPosition().Yi + 'px, 0px)'
-            point.style.transition = 'transform 800ms ease-in-out'
-            context.parent.$zircle.setNavigationMode('backward')
+            // container.style.transition = 'transform 800ms linear'
+            point.style.transition = 'transform 900ms ease-in'
             done()
           } else {
-            el.style.opacity = 1
             done()
           }
         },
         afterEnter (el) {
-          var point = document.getElementById('z-point')
-          point.style.willChange = ''
+          // var container = document.getElementById('z-p')
+          if (context.parent.$zircle.getNavigationMode() === 'forward') {
+            context.parent.$zircle.setNavigationMode('')
+          } else {
+          }
         },
         beforeLeave (el) {
         },
         leave (el, done) {
-          var point = document.getElementById('z-point')
           if (context.parent.$zircle.getNavigationMode() === 'forward') {
             done()
           } else {
-            point.style.willChange = 'transform'
+            var point = document.getElementById('z-point')
+            // var container = document.getElementById('z-p')
+            // container.style.transformOrigin = context.parent.$zircle.getLastPosition().X + 'px ' + context.parent.$zircle.getLastPosition().Y + 'px'
+            // container.style.transform = 'scale(' + context.parent.$zircle.getCurrentPosition().scale + ')'
             point.style.transform = 'scale(' + context.parent.$zircle.getCurrentPosition().scale + ') translate3d(' + context.parent.$zircle.getCurrentPosition().Xi + 'px, ' + context.parent.$zircle.getCurrentPosition().Yi + 'px, 0px)'
-            point.style.transition = 'transform 800ms ease-in-out'
-            el.classList.add('lastclass')
+            // container.style.transition = 'transform 800ms linear'
+            point.style.transition = 'transform 1000ms ease-in'
+            el.classList.add('disappear')
             setTimeout(function () {
               done()
-            }, 600)
+            }, 800)
           }
         },
         afterLeave (el) {
-          var point = document.getElementById('z-point')
-          point.style.willChange = ''
+          if (context.parent.$zircle.getNavigationMode() === 'forward') {
+          } else {
+          }
         }
       }
     }
