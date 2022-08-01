c5bbd52605928050e612fabd026ef47295d491f5
DiD <rhandy.grard@gmail.com>
2022-08-01 16:02:49 +0200

    refactor(tests): use findComponent instead of find deprecated method

--- tests/unit/ZView.spec.js
+++ tests/unit/ZView.spec.js
@@ -48,12 +48,12 @@ describe('z-view.vue', () => {
     expect(wrapper.vm.responsive).toEqual(true)
   })
   it('Has z-scroll component and longtext class activated because of scrollBar true', () => {
-    expect(wrapper.find('.maincontent').classes()).toContain('long-content')
-    expect(wrapper.find(zscroll).exists()).toBe(true)
+    expect(wrapper.findComponent('.maincontent').classes()).toContain('long-content')
+    expect(wrapper.findComponent(zscroll).exists()).toBe(true)
   })
   it('Has z-slider component and progress 80%', () => {
     expect(wrapper.vm.progress).toEqual(80)
-    expect(wrapper.find(zslider).exists()).toBe(true)
+    expect(wrapper.findComponent(zslider).exists()).toBe(true)
   })
   it('Has the expected html structure (slots, z-scroll, z-slider)', () => {
     // Note: If props.imagePath and or props.label are present, slots.imagesrc and slots.label wont be rendered
