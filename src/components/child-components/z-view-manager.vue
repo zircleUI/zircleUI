1e9d94d79d7ae6be6be31cb2efad1ebd50dcba44
DiD <rhandy.grard@gmail.com>
2022-08-05 19:52:51 +0200

    fix(responsiveness): add last sizes and positions fix with some refactoring

--- src/components/child-components/z-view-manager.vue
+++ src/components/child-components/z-view-manager.vue
@@ -4,21 +4,26 @@
       v-for="view in views"
       :is="view.component"
       :class="{
-        'is-current-view': $zircle.getCurrentViewName() === view.name,
+        'is-current-view': $zircle.getCurrentViewName() === view.name && $zircle.getRouterState() === false,
         'is-previous-view': $zircle.getPreviousViewName() === view.name,
         'is-past-view': $zircle.getPastViewName() === view.name
       }"
-      :key="view.uniqueKey">
-    </component>
+      :key="view.uniqueKey"/>
+    <router-view
+      v-if="$zircle.getRouterState() === true && $zircle.getHistoryLength() >= 1"
+      class="is-current-view"
+      :key="$zircle.getCurrentViewName()"/>
   </transition-group>
 </template>
 <script>
+/* eslint-disable */
 export default {
   name: 'z-view-manager',
   computed: {
     views () {
-      return this.$zircle.getHistory().slice(-3)
+      return this.$zircle.getRouterState() === false ? this.$zircle.getHistory().slice(-3) : this.$zircle.getHistory().slice(-3, -1)
     }
   }
 }
 </script>
+
