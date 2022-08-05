ae524bd7c148408cee437047847ab410e7874ac9
Martin Muda <mud@mrecic.gov.ar>
2022-08-05 13:32:32 -0300

    fix: zoom levels and element position

--- src/components/child-components/z-view-manager.vue
+++ src/components/child-components/z-view-manager.vue
@@ -1,15 +1,19 @@
 <template>
-  <transition-group :name="$zircle.getNavigationMode() === 'forward' ? 'z-next' : 'z-prev'" tag="section">
-    <component
-      v-for="(view, i) in views"
+   <transition-group :name="$zircle.getNavigationMode() === 'forward' ? 'z-next' : 'z-prev'" tag="section">
+      <component v-for="(view, i) in views"
       :is="view.component"
       :class="{
-        'is-current-view': $zircle.getCurrentViewName() === view.name,
+        'is-current-view': $zircle.getCurrentViewName() === view.name && $zircle.getRouterState() === false,
         'is-previous-view': $zircle.getPreviousViewName() === view.name,
         'is-past-view': $zircle.getPastViewName() === view.name
       }"
       :key="`${view.name}_${i}}`">
-    </component>
+      </component>
+      <router-view
+        v-if="$zircle.getRouterState() === true && $zircle.getHistoryLength() >= 1"
+        class="is-current-view"
+        :key="$zircle.getCurrentViewName()">
+      </router-view>
   </transition-group>
 </template>
 <script>
@@ -18,8 +22,9 @@ export default {
   name: 'z-view-manager',
   computed: {
     views () {
-      return this.$zircle.getHistory().slice(-3)
+      return this.$zircle.getRouterState() === false ? this.$zircle.getHistory().slice(-3) : this.$zircle.getHistory().slice(-3, -1)
     }
   }
 }
 </script>
+
