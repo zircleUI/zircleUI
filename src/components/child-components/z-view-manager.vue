f9c11aabc3f115057ce4800b7e278db30d70e213
Martin Muda <mud@mrecic.gov.ar>
2022-08-05 13:44:21 -0300

    fix: zoom out

--- src/components/child-components/z-view-manager.vue
+++ src/components/child-components/z-view-manager.vue
@@ -1,13 +1,13 @@
 <template>
    <transition-group :name="$zircle.getNavigationMode() === 'forward' ? 'z-next' : 'z-prev'" tag="section">
-      <component v-for="(view, i) in views"
+      <component v-for="view in views"
       :is="view.component"
       :class="{
         'is-current-view': $zircle.getCurrentViewName() === view.name && $zircle.getRouterState() === false,
         'is-previous-view': $zircle.getPreviousViewName() === view.name,
         'is-past-view': $zircle.getPastViewName() === view.name
       }"
-      :key="`${view.name}_${i}}`">
+      :key="view.name">
       </component>
       <router-view
         v-if="$zircle.getRouterState() === true && $zircle.getHistoryLength() >= 1"
