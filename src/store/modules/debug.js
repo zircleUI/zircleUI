7eb1bdbb93fb97622856a30bb0a2c52f309b1635
DiD <rhandy.grard@gmail.com>
2022-08-13 19:33:12 +0200

    refactor(debug): remove unused getState method in debug

--- src/store/modules/debug.js
+++ src/store/modules/debug.js
@@ -14,9 +14,6 @@ const debug = {
     if (store.state.debug) {
       console.log('%c z ', 'background: ' + bgColor + '; color:  ' + color + '', msg) // eslint-disable-line no-console
     }
-  },
-  getState () {
-    return store.state.$data
   }
 }
 export default debug
