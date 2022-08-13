be9fac2486e66aae0a39ae50355796992da5f31a
DiD <rhandy.grard@gmail.com>
2022-08-13 18:49:49 +0200

    refactor(computed-shape): return circle shape by default

--- src/components/z-dialog.vue
+++ src/components/z-dialog.vue
@@ -89,13 +89,10 @@ export default {
       )
     },
     shape () {
-      if (this.circle) {
-        return 'is-circle'
-      } else if (this.square) {
+      if (this.square) {
         return 'is-square'
-      } else {
-        return 'is-circle'
       }
+      return 'is-circle'
     },
     styles () {
       const zwidth = this.$zircle.getComponentWidth(this.size)
