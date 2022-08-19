d2455b5018d6f73ef74c146b517a0d6d8fe8807c
Martin Muda <mud@mrecic.gov.ar>
2022-08-19 14:35:10 -0300

    fix: re add getState()

--- src/store/modules/debug.js
+++ src/store/modules/debug.js
@@ -14,6 +14,9 @@ const debug = {
     if (store.state.debug) {
       console.log('%c z ', 'background: ' + bgColor + '; color:  ' + color + '', msg) // eslint-disable-line no-console
     }
+  },
+  getState () {
+    return store.state.$data
   }
 }
 export default debug
